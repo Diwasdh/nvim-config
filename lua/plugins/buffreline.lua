6c4ce74ed750e727056b51c7395c43257ec8970c
Hell <diwasdhakal75@gmail.com>
2025-08-28 05:13:33 +0000

    Vim bufferline

---
+++ lua/plugins/buffreline.lua
@@ -0,0 +1,21 @@
+return {
+  'akinsho/bufferline.nvim',
+  version = "*",
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    require("bufferline").setup{
+      options = {
+        diagnostics = "nvim_lsp",
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = "File Explorer",
+            highlight = "Directory",
+            //text aign after tree setter
+            text_align = "left"
+          }
+        }
+      }
+    }
+  end
+}
