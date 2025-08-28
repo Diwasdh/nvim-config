1789a279561a2ff6bb73c4c2c47333497f2d09c9
Hell <diwasdhakal75@gmail.com>
2025-08-28 09:56:17 +0000

    dashboard and other shortcut changes

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,11 +1,19 @@
 return {
-	"goolord/alpha-nvim",
-	dependencies = {
-		"echasnovski/mini.icons",
-		"nvim-lua/plenary.nvim",
-	},
+	"nvimdev/dashboard-nvim",
+	event = "VimEnter",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		local startify = require("alpha.themes.theta")
-		require("alpha").setup(startify.config)
+		local db = require("dashboard")
+		db.setup({
+			theme = "doom",
+			config = {
+				center = {
+					{ icon = "  ", desc = "Find File                  ", action = "Telescope find_files" },
+					{ icon = "  ", desc = "File Explorer              ", action = "Neotree toggle filesystem left" },
+					{ icon = "  ", desc = "Find Word                  ", action = "Telescope live_grep" },
+					{ icon = "  ", desc = "Quit                       ", action = "qa" },
+				},
+			},
+		})
 	end,
 }
