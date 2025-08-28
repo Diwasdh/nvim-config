55a407615f2a6aa72cc4c7e26d836df1fec06ccc
Hell <diwasdhakal75@gmail.com>
2025-08-28 10:13:19 +0000

    dashboard icon change

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,19 +1,23 @@
 return {
-	"nvimdev/dashboard-nvim",
-	event = "VimEnter",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		local db = require("dashboard")
-		db.setup({
-			theme = "doom",
-			config = {
-				center = {
-					{ icon = "  ", desc = "Find File                  ", action = "Telescope find_files" },
-					{ icon = "  ", desc = "File Explorer              ", action = "Neotree toggle filesystem left" },
-					{ icon = "  ", desc = "Find Word                  ", action = "Telescope live_grep" },
-					{ icon = "  ", desc = "Quit                       ", action = "qa" },
-				},
-			},
-		})
-	end,
+  "nvimdev/dashboard-nvim",
+  event = "VimEnter",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local db = require("dashboard")
+    db.setup({
+      theme = "doom",
+      config = {
+        center = {
+          { icon = "   ", desc = "Find File                  ", action = "Telescope find_files" },
+          {
+            icon = "   ",
+            desc = "File Explorer              ",
+            action = "Neotree toggle filesystem left",
+          },
+          { icon = "   ", desc = "Find Word                  ", action = "Telescope live_grep" },
+          { icon = "⏻   ", desc = "Quit                       ", action = "qa" },
+        },
+      },
+    })
+  end,
 }
